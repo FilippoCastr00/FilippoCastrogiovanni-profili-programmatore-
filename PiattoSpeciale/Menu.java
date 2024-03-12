beb30181e37fb3ea33ec9185f9df1b10834c8419
FilippoCastr00 <153097529+FilippoCastr00@users.noreply.github.com>
2024-03-12 16:37:20 +0100

    Aggiunto print menu e variabili prezzi
    16:35 Castrogiovanni Ho aggiunto un print al menu e variabili prezzo a piatto speciale

--- PiattoSpeciale/Menu.java
+++ PiattoSpeciale/Menu.java
@@ -1,5 +1,13 @@
 package PiattoSpeciale;
 
 public class Menu {
-    
+    public void menu(){
+      System.out.println("Benvenuto!!");
+      System.out.println("1. Seleziona tipo di pane");
+      System.out.println("2. Vedi lista ingredienti");
+      
+    }
+    public static void main(String[] args) {
+        Menu myMenu = new Menu();
+    }
 }
--- PiattoSpeciale/PiattoSpeciale.java
+++ PiattoSpeciale/PiattoSpeciale.java
@@ -1,22 +1,28 @@
 package PiattoSpeciale;
 
+import java.util.ArrayList;
+
 public class PiattoSpeciale {
     // Variabili final
         final String panino1;
         final String panino2;
-
+        private int[] prezzoPane;
+        
     // Variabili pubbliche
     public String ingrediente1;
     public String ingrediente2;
     public String ingrediente3;
+    public ArrayList prezzoIngredienti = new ArrayList<Integer>();
 
     // Costruttore
-    public PiattoSpeciale(String panino1, String panino2, String ingrediente1, String ingrediente2, String ingrediente3) {
+    public PiattoSpeciale(String panino1, String panino2,int[] prezzoPane ,String ingrediente1, String ingrediente2, String ingrediente3,ArrayList prezzoIngredienti) {
         this.panino1 = panino1;
         this.panino2 = panino2;
+        this.prezzoPane= prezzoPane;
         this.ingrediente1 = ingrediente1;
         this.ingrediente2 = ingrediente2;
         this.ingrediente3 = ingrediente3;
+        this.prezzoIngredienti = prezzoIngredienti;
     }
 
     // Metodi per accedere alle variabili final
@@ -27,6 +33,13 @@ public class PiattoSpeciale {
     public String getPanino2() {
         return panino2;
     }
+    public int[] getPrezzoPane(){
+        return prezzoPane;
+    }
+    public ArrayList getPrezzoIngredienti(){
+        
+        return prezzoIngredienti;
+    } 
     
 }
 
