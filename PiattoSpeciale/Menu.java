73907408e04a1ba7c6ab5a7ae8ac8be1c2e8bc24
FilippoCastr00 <153097529+FilippoCastr00@users.noreply.github.com>
2024-03-13 14:02:08 +0100

    Aggiunta scelta indicizzata e manuale
    14:00 Castrogiovanni Ho aggiunto alla classe Menu la scelta indicizzata per i public e quella manuale per i private

--- PiattoSpeciale/Menu.java
+++ PiattoSpeciale/Menu.java
@@ -4,31 +4,40 @@ import java.util.Scanner;
 
 public class Menu {
     // Metodo per stampare il menu con le variabili final
-    public static void menuStatic(String panino1, String panino2) {
-        System.out.println("---- Menu ----");
-        System.out.println("1. " + panino1);
-        System.out.println("2. " + panino2);
+    public static void menuStatic(String panino1, String panino2, int prezzoPane1, int prezzoPane2) {
+        System.out.println("---- Base pane obbligatoria ----");
+        System.out.println("1. " + panino1 + " " + prezzoPane1+ " " +"euro" );
+        System.out.println("2. " + panino2 + " " + prezzoPane2+" " +"euro");
         System.out.println("--------------");
     }
      // Metodo per stampare il menu con gli ingredienti pubblici
-     public static void menuPublic(String ingrediente1, String ingrediente2, String ingrediente3) {
+     public static void menuPublic(String ingrediente1, String ingrediente2, String ingrediente3,int prezzoIng1,int prezzoIng2,int prezzoIng3) {
         System.out.println("---- Ingredienti Disponibili ----");
-        System.out.println("1. " + ingrediente1);
-        System.out.println("2. " + ingrediente2);
-        System.out.println("3. " + ingrediente3);
+        System.out.println("1. " + ingrediente1 + " " + prezzoIng1 +" " + "euro");
+        System.out.println("2. " + ingrediente2 + " " + prezzoIng2 +" " + "euro");
+        System.out.println("3. " + ingrediente3 + " " + prezzoIng3 +" " + "euro");
         System.out.println("-------------------------");
     }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //creo l'oggetto nuovoPiatto
-        PiattoSpeciale nuovoPiatto = new PiattoSpeciale("Semola", "Cereali", null, null, null);
+        PiattoSpeciale nuovoPiatto = new PiattoSpeciale("Panino Semola", "Panino Cereali",5,7, "Prosciutto", "Salsiccia", "Mozzarella",6,7,8);
         //ciclo per vedere il menu panini
         while (true) {
             System.out.println("Vuoi vedere il menu panini? (si/no)");
             String risposta = scanner.nextLine().toLowerCase();
-            if (risposta.equals("si")) {                              //aggiungere inserimento manuale
-                menuStatic(nuovoPiatto.getPanino1(), nuovoPiatto.getPanino2());
+            if (risposta.equals("si")) {                              
+                menuStatic(nuovoPiatto.getPanino1(), nuovoPiatto.getPanino2(), nuovoPiatto.getPrezzoPane1(), nuovoPiatto.getPrezzoPane2());
+                System.out.println("Scegli uno: ");
+                //inserimento manuale
+                String sceltaPane = scanner.nextLine();
+                if (sceltaPane.equals("Panino Semola")) {
+                    System.out.println("Hai scelto base semola "); 
+                    // aggiungere inserimento in array per il prezzo e ingredienti
+                }else if (sceltaPane.equals("Panino Cereali")) {
+                    System.out.println("Hai scelto base cereali ");
+                }
                 break;
             } else if (risposta.equals("no")) {
                 break;
@@ -42,8 +51,23 @@ public class Menu {
             String risposta = scanner.nextLine().toLowerCase();
             
             if (risposta.equals("si")) {
-                menuPublic(nuovoPiatto.ingrediente1, nuovoPiatto.ingrediente2, nuovoPiatto.ingrediente3);
+                menuPublic(nuovoPiatto.ingrediente1, nuovoPiatto.ingrediente2, nuovoPiatto.ingrediente3,nuovoPiatto.prezzoIng1,nuovoPiatto.prezzoIng2,nuovoPiatto.prezzoIng3);
+                System.out.println("Scegli inserendo da 1 a 3: ");
+                int sceltaIngr = scanner.nextInt();
                 //aggiungere inserimento indicizzato
+                switch (sceltaIngr) {
+                    case 1:
+                           System.out.println("Hai scelto Prosciutto");
+                        break;
+                    case 2:
+                           System.out.println("Hai scelto Salsiccia");
+                        break;
+                    case 3:
+                           System.out.println("Hai scelto Mozzarella");           
+                
+                    default:
+                        break;
+                }
                 break;
             } else if (risposta.equals("no")) {
                 break;
--- PiattoSpeciale/PiattoSpeciale.java
+++ PiattoSpeciale/PiattoSpeciale.java
@@ -6,19 +6,32 @@ public class PiattoSpeciale {
     // Variabili final
     private final String panino1;
     private final String panino2;
+    private final int prezzoPane1;
+    private final int prezzoPane2;
+
 
     // Variabili pubbliche
     public String ingrediente1;
     public String ingrediente2;
     public String ingrediente3;
+    public int prezzoIng1;
+    public int prezzoIng2;
+    public int prezzoIng3;
+
 
     // Costruttore
-    public PiattoSpeciale(String panino1, String panino2, String ingrediente1, String ingrediente2, String ingrediente3) {
+    public PiattoSpeciale(String panino1, String panino2,int prezzoPane1,int prezzoPane2, String ingrediente1, String ingrediente2, String ingrediente3,int prezzoIng1,int prezzoIng2,int prezzoIng3) {
         this.panino1 = panino1;
         this.panino2 = panino2;
+        this.prezzoPane1 = prezzoPane1;
+        this.prezzoPane2 = prezzoPane2;
         this.ingrediente1 = ingrediente1;
         this.ingrediente2 = ingrediente2;
         this.ingrediente3 = ingrediente3;
+        this.prezzoIng1 = prezzoIng1;
+        this.prezzoIng2 = prezzoIng2;
+        this.prezzoIng3 = prezzoIng3;
+
     }
 
     // Metodo per ottenere il nome del primo panino
@@ -29,6 +42,12 @@ public class PiattoSpeciale {
     // Metodo per ottenere il nome del secondo panino
     public String getPanino2() {
         return panino2;
+    }
+    public int getPrezzoPane1(){
+        return prezzoPane1;
+    }
+    public int getPrezzoPane2(){
+        return prezzoPane2;
     }
 
     // Metodo per stampare lo stato dell'oggetto
@@ -42,7 +61,7 @@ public class PiattoSpeciale {
    
     // Metodo main per testare la classe
     public static void main(String[] args) {
-        PiattoSpeciale panini = new PiattoSpeciale("Panino Vegetariano", "Panino con Prosciutto", "Insalata", "Pomodoro", "Formaggio");
+        PiattoSpeciale panini = new PiattoSpeciale("Panino Semola", "Panino Cereali",5,7, "Prosciutto", "Salsiccia", "Mozzarella",6,8,7);
         panini.printStatus();
     }
 }
